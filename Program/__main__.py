ff9caf15ddb7d7eb78c6cd533f32b1b7d47702ef
DiscoMike <samoulov.max.18@gmail.com>
2022-12-26 14:45:38 +0300

    added single jpg convertion

--- Program/__main__.py
+++ Program/__main__.py
@@ -3,32 +3,28 @@ import img2pdf
 import cv2
 import os
 
+THRESHOLD = 120
+
 def main():
     print()
     print('Welcome!')
-    # lst = [0, 1, 10, 22, 222]
-    # for i in lst:
-    #     zero_padded_i = str(i) + '0' * (4 - len(str(i)))
-    #     print(zero_padded_i)
-    file_name = get_file_name()
+
+    print('First, put your pdf file into "input-files" folder.')
+    file_name = input('Then, enter your pdf file name: ')
     print()
     print('[Starting...]')
-
-    file = convert_from_path(f'Program\input-files\{file_name}', 300)
-    convert_pdf2jpg(file)
-    apply_binary_to_images()
-    convert_images2pdf(result_name=file_name)
+    if file_name.endswith('.jpg'):
+        convert_single_image(file_name)
+    else:
+        file = convert_from_path(f'Program\input-files\{file_name}', 300)
+        convert_pdf2jpg(file)
+        apply_binary_to_images()
+        convert_images2pdf(result_name=file_name)
 
     print()
     print('Done! You can now get your file from "output-files" folder.')
     print()
 
-def get_file_name():
-    print('First, put your pdf file into "input-files" folder.')
-    file_name = input('Then, enter your pdf file name: ')
-    if not file_name.endswith('.pdf'):
-       file_name += '.pdf'
-    return file_name
 
 def convert_pdf2jpg(file):
     file_length = len(file)
@@ -44,8 +40,6 @@ def convert_pdf2jpg(file):
 def convert_images2pdf(result_name: str):
     with open(f'Program\output-files\{result_name}', 'wb') as result:
         pages_list = [f'Program\processed-files\{file}' for file in os.listdir('Program\processed-files')]
-        # pages_list.sort(key=lambda f: int(f.replace('.jpg', '').split('page')[1]))
-        print(pages_list)
         result.write(img2pdf.convert(pages_list))
     # Delete obsolete middle files
     path = 'Program\\processed-files\\'
@@ -55,15 +49,21 @@ def convert_images2pdf(result_name: str):
 
 def apply_binary_to_images():
     # Apply same binary scale to each file in processed-files folder
-    for file in os.listdir('Program\processed-files'):
-        path = f'Program\processed-files\{file}'
-
+    for file_name in os.listdir('Program\processed-files'):
+        path = f'Program\processed-files\{file_name}'
         originalImage = cv2.imread(path)
         grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
-        (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 170, 255, cv2.THRESH_BINARY)
-        index = file.replace('.jpg', '').split('page')[1]
+        (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, THRESHOLD, 255, cv2.THRESH_BINARY)
+        index = file_name.replace('.jpg', '').split('page')[1]
         cv2.imwrite(f'Program\processed-files\page{index}.jpg', blackAndWhiteImage)
     print('[Successfully applied black-and-white filter]')
+
+def convert_single_image(file_name):
+    path = f'Program\input-files\{file_name}'
+    originalImage = cv2.imread(path)
+    grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
+    (thresh, BinaryImage) = cv2.threshold(grayImage, THRESHOLD, 255, cv2.THRESH_BINARY)
+    cv2.imwrite(f'Program\output-files\{file_name}', BinaryImage)
 
 if __name__ == '__main__':
     main()
